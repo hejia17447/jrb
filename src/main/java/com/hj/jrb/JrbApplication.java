27fa31f757ca682af41c89b7dd79a543bb3940a0
贺佳 <hejia1744709356@163.com>
2023-05-31 00:26:13 +0800

    设置依赖版本

---
+++ src/main/java/com/hj/jrb/JrbApplication.java
@@ -0,0 +1,13 @@
+package com.hj.jrb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JrbApplication.class, args);
+    }
+
+}
